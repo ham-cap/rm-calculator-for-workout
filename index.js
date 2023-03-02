1fabf53d34f062d0f8d82276bd88b9c5e1ce26ff
Toshiyasu Nonohara <hh0648075@gmail.com>
2023-03-02 16:09:20 +0900

    recommendTrainingMenu関数を仮実装

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const { Select } = require("enquirer");
 const { Form } = require("enquirer");
 const COEFFICIENT_OF_BENCH_PRESS = 40;
+const COEFFICIENT_FOR_STRENGTH = 0.87;
+const NUMBER_OF_REPETITION_FOR_STRENGTH = 5;
+const COEFFICIENT_FOR_HYPERTROPHY = 0.8;
+const NUMBER_OF_REPETITION_FOR_HYPERTROPHY = 8;
+const COEFFICIENT_FOR_ENDURANCE = 0.65;
+const NUMBER_OF_REPETITION_FOR_ENDURANCE = 18;
+const NUMBER_OF_SETS = 3;
+
+const recommendTrainingMenu = function (rm) {
+  console.log("目的別の推奨メニューは以下のとおりです。");
+  console.log(
+    `筋力アップ： ${Math.round(
+      rm * COEFFICIENT_FOR_STRENGTH
+    )}kg × ${NUMBER_OF_REPETITION_FOR_STRENGTH}回 × ${NUMBER_OF_SETS}セット`
+  );
+  console.log(
+    `筋肥大： ${Math.round(
+      rm * COEFFICIENT_FOR_HYPERTROPHY
+    )}kg × ${NUMBER_OF_REPETITION_FOR_HYPERTROPHY}回 × ${NUMBER_OF_SETS}セット`
+  );
+  console.log(
+    `筋持久力アップ： ${Math.round(
+      rm * COEFFICIENT_FOR_ENDURANCE
+    )}kg × ${NUMBER_OF_REPETITION_FOR_ENDURANCE}回 × ${NUMBER_OF_SETS}セット`
+  );
+  console.log("理想の身体を目指して頑張りましょう！");
+};
 
 const calculateRm = function (weightAndReps) {
   const weight = parseFloat(weightAndReps.weight);
   const reps = parseFloat(weightAndReps.reps);
-  console.log(weight);
-  console.log(reps);
   const rm = (weight * reps) / COEFFICIENT_OF_BENCH_PRESS + weight;
   console.log(`あなたの現在の1RMは「 ${Math.round(rm)}kg 」です。`);
   console.log(
@@ -15,6 +40,7 @@ const calculateRm = function (weightAndReps) {
   console.log(
     "※注2：結果はあくまでも目安です。実際に扱える重量には個人差があります。"
   );
+  recommendTrainingMenu(rm);
 };
 
 const isPositiveNumber = function (input) {
