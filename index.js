a468b33e07c6bfebe3d8b4d43f7f1f9d5cb903a8
Toshiyasu Nonohara <hh0648075@gmail.com>
2023-03-05 09:57:15 +0900

    BenchPressクラスを作成

--- index.js
+++ index.js
@@ -1,47 +1,6 @@
 const { Select } = require("enquirer");
 const { Form } = require("enquirer");
-const COEFFICIENT_OF_BENCH_PRESS = 40;
-const COEFFICIENT_FOR_STRENGTH = 0.87;
-const NUMBER_OF_REPETITION_FOR_STRENGTH = 5;
-const COEFFICIENT_FOR_HYPERTROPHY = 0.8;
-const NUMBER_OF_REPETITION_FOR_HYPERTROPHY = 8;
-const COEFFICIENT_FOR_ENDURANCE = 0.65;
-const NUMBER_OF_REPETITION_FOR_ENDURANCE = 18;
-const NUMBER_OF_SETS = 3;
-
-const recommendTrainingMenu = function (rm) {
-  console.log("目的別の推奨メニューは以下のとおりです。");
-  console.log(
-    `筋力アップ： ${Math.round(
-      rm * COEFFICIENT_FOR_STRENGTH
-    )}kg × ${NUMBER_OF_REPETITION_FOR_STRENGTH}回 × ${NUMBER_OF_SETS}セット`
-  );
-  console.log(
-    `筋肥大： ${Math.round(
-      rm * COEFFICIENT_FOR_HYPERTROPHY
-    )}kg × ${NUMBER_OF_REPETITION_FOR_HYPERTROPHY}回 × ${NUMBER_OF_SETS}セット`
-  );
-  console.log(
-    `筋持久力アップ： ${Math.round(
-      rm * COEFFICIENT_FOR_ENDURANCE
-    )}kg × ${NUMBER_OF_REPETITION_FOR_ENDURANCE}回 × ${NUMBER_OF_SETS}セット`
-  );
-  console.log("理想の身体を目指して頑張りましょう！");
-};
-
-const calculateRm = function (weightAndReps) {
-  const weight = parseFloat(weightAndReps.weight);
-  const reps = parseFloat(weightAndReps.reps);
-  const rm = (weight * reps) / COEFFICIENT_OF_BENCH_PRESS + weight;
-  console.log(`あなたの現在の1RMは「 ${Math.round(rm)}kg 」です。`);
-  console.log(
-    "※注1：1RMとは「1 Repetition Maximum（最大挙上重量）」の略称であり、1回しか持ち上げられない重量を指します。"
-  );
-  console.log(
-    "※注2：結果はあくまでも目安です。実際に扱える重量には個人差があります。"
-  );
-  recommendTrainingMenu(rm);
-};
+const BenchPress = require("./bench_press.js");
 
 const isPositiveNumber = function (input) {
   const [weight, reps] = Object.values(input).map((value) => parseFloat(value));
@@ -53,8 +12,10 @@ const isPositiveNumber = function (input) {
     return "Please input valid numbers.";
   }
 };
+
 const askWeightAndReps = function (answer) {
   console.log(`Answer:${answer}`);
+  const selectedExercise = new BenchPress();
   const promptEnterWeightAndReps = new Form({
     name: "user",
     message: "Please provide the following information:",
@@ -73,7 +34,7 @@ const askWeightAndReps = function (answer) {
 
   promptEnterWeightAndReps
     .run()
-    .then((weightAndReps) => calculateRm(weightAndReps))
+    .then((weightAndReps) => selectedExercise.calculateRm(weightAndReps))
     .catch(console.error);
 };
 
