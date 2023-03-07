ff84a12e77dfeb484c9e47d2c5aeffd28422f1f7
Toshiyasu Nonohara <hh0648075@gmail.com>
2023-03-07 15:14:23 +0900

    目的別の推奨メニュー表示における使用重量の計算部分をCalculatorクラスへ切り出した
    ・何かを計算する処理は全てCalculatorクラスの責務としたいため

--- exercise.js
+++ exercise.js
@@ -3,11 +3,8 @@ const calculator = new Calculator();
 
 module.exports = class Exercise {
   constructor() {
-    this.COEFFICIENT_FOR_STRENGTH = 0.87;
     this.NUMBER_OF_REPETITION_FOR_STRENGTH = 5;
-    this.COEFFICIENT_FOR_HYPERTROPHY = 0.8;
     this.NUMBER_OF_REPETITION_FOR_HYPERTROPHY = 8;
-    this.COEFFICIENT_FOR_ENDURANCE = 0.65;
     this.NUMBER_OF_REPETITION_FOR_ENDURANCE = 18;
     this.NUMBER_OF_SETS = 3;
   }
@@ -23,23 +20,17 @@ module.exports = class Exercise {
   }
 
   recommendTrainingMenu(rm) {
+    const recommendedWeight = calculator.calculateRecommendedWeight(rm);
+    console.log("");
     console.log("目的別の推奨メニューは以下のとおりです。");
     console.log(
-      `【筋力アップ】 ${Math.round(rm * this.COEFFICIENT_FOR_STRENGTH)}kg × ${
-        this.NUMBER_OF_REPETITION_FOR_STRENGTH
-      }回 × ${this.NUMBER_OF_SETS}セット`
+      `【筋力アップ】 ${recommendedWeight.for_strength}kg × ${this.NUMBER_OF_REPETITION_FOR_STRENGTH}回 × ${this.NUMBER_OF_SETS}セット`
     );
     console.log(
-      `【筋肥大】 ${Math.round(rm * this.COEFFICIENT_FOR_HYPERTROPHY)}kg × ${
-        this.NUMBER_OF_REPETITION_FOR_HYPERTROPHY
-      }回 × ${this.NUMBER_OF_SETS}セット`
+      `【筋肥大】 ${recommendedWeight.for_hypertrophy}kg × ${this.NUMBER_OF_REPETITION_FOR_HYPERTROPHY}回 × ${this.NUMBER_OF_SETS}セット`
     );
     console.log(
-      `【筋持久力アップ】 ${Math.round(
-        rm * this.COEFFICIENT_FOR_ENDURANCE
-      )}kg × ${this.NUMBER_OF_REPETITION_FOR_ENDURANCE}回 × ${
-        this.NUMBER_OF_SETS
-      }セット`
+      `【筋持久力アップ】 ${recommendedWeight.for_endurance}kg × ${this.NUMBER_OF_REPETITION_FOR_ENDURANCE}回 × ${this.NUMBER_OF_SETS}セット`
     );
     console.log("理想の身体を目指して頑張りましょう！");
   }
